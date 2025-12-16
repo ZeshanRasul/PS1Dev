ab26091269f37b3514f669a8e5d8ec26607f6333
Zeshan Rasul <zeshanrasul90@gmail.com>
2025-12-16 15:37:07 +0000

    Create subroutine to draw flat shaded triangles

--- hellogpu/hellogpu.s
+++ hellogpu/hellogpu.s
@@ -65,17 +65,7 @@ Main:
     li $t1, 0x00EF013F
     sw $t1, GP0($t0)
 
-    ;;; Draw Flat Shaded Triangle
-    li $t1, 0x2014F7FF
-    sw $t1, GP0($t0)
-    li $t1, 0x005000A0
-    sw $t1, GP0($t0)
-    li $t1, 0x00A000F0
-    sw $t1, GP0($t0)
-    li $t1, 0x00A00050
-    sw $t1, GP0($t0)
-
-    ;;; Draw Flay Shaded Quad
+    ;;; Draw Flat Shaded Quad
     li $t1, 0x2803DE33
     sw $t1, GP0($t0)
     li $t1, 0x00A00050
@@ -87,8 +77,56 @@ Main:
     li $t1, 0x00F000F0
     sw $t1, GP0($t0)
 
+    ;;; Draw Flat Shaded Triangle
+    lui $a0, IO_BASE_ADDR
+    li $s0, 0xFFFF00
+    li $s1, 200
+    li $s2, 40
+    li $s3, 288
+    li $s4, 56
+    li $s5, 224
+    li $s6, 200
+    jal DrawFlatShadedTriangle
+    nop
+
+
 LoopForever:
     j LoopForever
+    nop
+
+;;;;;;;;;;
+;; Subroutine to draw flat shaded triangle
+;; Args:
+;; $a0 = IO_BASE_ADDR
+;; $s0 = Color
+;; $s1 = x1
+;; $s2 = y1
+;; $s3 = x2
+;; $s4 = y2
+;; $s5 = x3
+;; $s6 = y3
+;;;;;;;;;;
+DrawFlatShadedTriangle:
+    lui $t0, 0x2000
+    or $t1, $t0, $s0
+    sw $t1, GP0($a0)
+
+    sll $s2, $s2, 16
+    andi $s1, $s1, 0xFFFF
+    or $t1, $s1, $s2
+    sw $t1, GP0($a0)
+
+    sll $s4, $s4, 16
+    andi $s3, $s3, 0xFFFF
+    or $t1, $s3, $s4
+    sw $t1, GP0($a0)
+
+    sll $s6, $s6, 16
+    andi $s5, $s5, 0xFFFF
+    or $t1, $s5, $s6
+    sw $t1, GP0($a0)
+
+    jr $ra
     nop
 
 End:
